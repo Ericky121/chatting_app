e9d3134a2d9c62c9547bd83e2057c2accc7c5322
Ericky121 <ericky@naver.com>
2021-12-31 15:52:40 +0900

    Main Changed

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() async {
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
-
+// MainClass
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
